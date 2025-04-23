6b7d288882568d8e3d969b3483ee6cb434edcd88
cactusMP4 <zsemen50@gmail.com>
2025-04-23 18:21:59 +0300

    make choosable input device in conf.json

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import sounddevice as sd
 import soundfile as sf
 import json
 
+config = json.load(open("conf.json"))
+
+def findDeviceIndex(name):
+    devices = sd.query_devices()
+    for index, device in enumerate(devices):
+        if name in device["name"] and device["max_input_channels"] > 0:
+            return index
+    return None
+
 def playSound(filename):
     data, samplerate = sf.read("sounds/"+filename)
-    sd.play(data, samplerate, device=43)
+
+    device = findDeviceIndex("CABLE Input (VB-Audio Virtual Cable), Windows DirectSound (0 in, 16 out)")
+
+    sd.play(data, samplerate, device=device)
     sd.wait()
 
-sounds = json.load(open("conf.json"))
-keyboard.add_hotkey(
-    "alt+1", lambda: playSound(sounds["1"])
-)
+keyboard.add_hotkey("alt+1", lambda: playSound(config["sounds"]["1"]),)
 
 keyboard.wait()
