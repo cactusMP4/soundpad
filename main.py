7c1e1a8f1063497ec9bd75f18da683384a6204e4
cactusMP4 <zsemen50@gmail.com>
2025-04-23 17:39:20 +0300

    finished

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import keyboard
+import sounddevice as sd
+import soundfile as sf
 
-def playSound(a):
-    print(a)
+def play_audio(filename):
+    data, samplerate = sf.read("sounds/"+filename)
+    sd.play(data, samplerate, device=43)
+    sd.wait()
 
 keyboard.add_hotkey(
-    "alt+1", lambda: playSound("abs")
+    "alt+1", lambda: play_audio("sfx_taunt.mp3")
 )
 
 keyboard.wait()
