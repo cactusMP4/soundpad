756dd8f16c5b5293051f41332bfe9150f854299a
cactusMP4 <zsemen50@gmail.com>
2025-04-24 16:58:30 +0300

    settings

--- main.py
+++ main.py
@@ -3,23 +3,15 @@ import sounddevice as sd
 import soundfile as sf
 import json
 
-config = json.load(open("conf.json"))
 
-def findDeviceIndex(name):
-    devices = sd.query_devices()
-    for index, device in enumerate(devices):
-        if name in device["name"] and device["max_input_channels"] > 0:
-            return index
-    return None
+def playSound(soundIndex):
+    config = json.load(open("conf.json"))
 
-def playSound(filename):
+    filename = config["sounds"][str(soundIndex)]
     data, samplerate = sf.read("sounds/"+filename)
 
-    device = findDeviceIndex("CABLE Input (VB-Audio Virtual Cable), Windows DirectSound (0 in, 16 out)")
+    sd.play(data, samplerate, device=config["deviceIndex"])
 
-    sd.play(data, samplerate, device=device)
-    sd.wait()
-
-keyboard.add_hotkey("alt+1", lambda: playSound(config["sounds"]["1"]),)
+keyboard.add_hotkey("alt+1", lambda: playSound(1),)
 
 keyboard.wait()
---
+++ settings.py
@@ -0,0 +1,52 @@
+import json
+import tkinter as tk
+from tkinter import ttk
+from sounddevice import query_devices
+
+
+def getIndexFromItem(item: str):
+    goingThroNumbers = False
+    index = ""
+    for char in item:
+        if char == "]":
+            return int(index)
+        if goingThroNumbers:
+            index += char
+        if char == "[":
+            goingThroNumbers = True
+
+
+def select():
+    selected_item = combo_box.get()
+
+    jsonObj = json.load(open("conf.json"))
+    jsonObj["deviceIndex"] = getIndexFromItem(selected_item)
+
+    with open("conf.json", "w") as outfile:
+        outfile.write(json.dumps(jsonObj))
+
+root = tk.Tk()
+root.resizable = True
+root.title("Combobox Example")
+
+# Create a label
+label = tk.Label(root, text="Selected Item: ")
+label.place(relx=0, rely=0, relwidth=1, relheight=0.2)
+label.config(text="Select Device: ")
+
+# Create a Combobox widget
+values = []
+for device in query_devices():
+    values.insert(len(values)+1, "["+str(device["index"])+"] "+device["name"])
+
+combo_box = ttk.Combobox(root, values=values)
+combo_box.pack(pady=5)
+combo_box.place(relx=0, rely=0.2, relwidth=1)
+
+# Set default value
+combo_box.set("🎃")
+
+# Bind event to selection
+combo_box.bind("<<ComboboxSelected>>", select)
+
+root.mainloop()
